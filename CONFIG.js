f68e668bfbb621941fc6720161d2aa7a43939bdb
TriAnh2212 <anhtri12a16@gmail.com>
2024-10-22 02:35:23 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -4,7 +4,7 @@ const CONFIGDATA = {
     "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
   buttonYes: "Tớ đồng ý",
   buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+  titleModar: "Lêu lêu cậu",
   descriptionModar:
-    "Cảm ơn InNhi đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ InNhi. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối mai tớ sẽ đến đón cậu đi gắp thú nhé. Cậu mặc gì cũng đẹp và có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Lêu lêu InNhi đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ InNhi. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối mai tớ sẽ đến đón cậu đi gắp thú nhé. Cậu mặc gì cũng đẹp và có một bất ngờ dành cho cậu.</br> Love you <3",
 };
